af98ee51c60620e4fe8b10efb3fdf0e661c030b7
John Doe <semre.kurt@gmail.com>
2020-07-22 12:48:44 -0400

    bug fix

--- inc/mutex.h
+++ inc/mutex.h
@@ -1,14 +1,19 @@
 #ifndef MUTEX_H
 #define MUTEX_H
+#include "../inc/util.h"
+
 #ifdef OMP
 #include <omp.h>
-#include "../inc/util.h"
+#endif
 typedef struct
 {
   bool initialized;
   int num_locks;
   int pad_size;
+  #ifdef OMP
   omp_lock_t * locks;
+  #endif
+  int* locks;
 } mutex_array;
 
 
@@ -27,18 +32,22 @@ static inline int mutex_translate_id(int const id, int const num_locks,int const
 static inline void mutex_set_lock(mutex_array * const array,int const id)
 {
   int const lock_id = mutex_translate_id(id, array->num_locks, array->pad_size);
+  #ifdef OMP
   omp_set_lock(array->locks + lock_id);
+  #endif
 }
 
 static inline void mutex_unset_lock(mutex_array * const array,int const id)
 {
   int const lock_id = mutex_translate_id(id, array->num_locks, array->pad_size);
+  #ifdef OMP
   omp_unset_lock(array->locks + lock_id);
+  #endif
 }
 
 
 
 
 
-#endif
+
 #endif
