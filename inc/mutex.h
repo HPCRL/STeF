ce9fb711a341e77f1ea851d938244d558e55e8eb
John Doe <semre.kurt@gmail.com>
2020-07-22 03:33:28 -0400

    mutex files added

---
+++ inc/mutex.h
@@ -0,0 +1,44 @@
+#ifndef MUTEX_H
+#define MUTEX_H
+#ifdef OMP
+#include <omp.h>
+
+typedef struct
+{
+  bool initialized;
+  int num_locks;
+  int pad_size;
+  omp_lock_t * locks;
+} mutex_array;
+
+
+mutex_array * mutex_alloc_custom(int const num_locks,int const pad_size);
+
+void mutex_free(mutex_array * array);
+
+
+
+static inline int mutex_translate_id(int const id, int const num_locks,int const pad_size)
+{
+  return (id % num_locks) * pad_size;
+}
+
+
+static inline void mutex_set_lock(mutex_array * const array,int const id)
+{
+  int const lock_id = mutex_translate_id(id, array->num_locks, array->pad_size);
+  omp_set_lock(array->locks + lock_id);
+}
+
+static inline void mutex_unset_lock(mutex_array * const array,int const id)
+{
+  int const lock_id = mutex_translate_id(id, array->num_locks, array->pad_size);
+  omp_unset_lock(array->locks + lock_id);
+}
+
+
+
+
+
+#endif
+#endif
---
+++ src/mutex.cpp
@@ -0,0 +1,24 @@
+#ifndef MUTEX_CPP
+#define MUTEX_CPP
+#ifdef OMP
+#include "../inc/mutex.h"
+
+mutex_array * mutex_alloc_custom(int const num_locks,int const pad_size)
+{
+	mutex_array * array = (mutex_array *) malloc(sizeof(mutex_array));
+
+	array->num_locks = num_locks;
+	array->pad_size = pad_size;
+
+
+	array->locks = (omp_lock_t *) malloc(num_locks * pad_size * sizeof(*array->locks));
+	for(int l=0; l < num_locks; ++l) 
+	{
+		int const lock = mutex_translate_id(l, num_locks, pad_size);
+		omp_init_lock(array->locks + lock);
+	}
+
+	return array;
+}
+#endif
+#endif
