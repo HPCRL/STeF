e5d3122e95eaf3c50a0e69945accce63b343f0e2
John Doe <semre.kurt@gmail.com>
2020-07-24 01:41:47 -0400

    performance improvement

--- src/mttkrp_hardwired.cpp
+++ src/mttkrp_hardwired.cpp
@@ -5,20 +5,38 @@
 int reduce(csf* t, int r, matrix* mat)
 {
 	memset (mat->val, 0 , sizeof(TYPE)*(mat->dim1)*(mat->dim2));
+	int parallel_threshold = 1000;
 	for(int i=0; i<t->num_th ; i++)
 	{
-
-		#pragma omp parallel for
-		for(int row_id = 0; row_id < mat->dim1 ; row_id++)
+		if (mat->dim1 > parallel_threshold)
 		{
-			TYPE* outval = mat->val + row_id * (mat->dim2);
-			TYPE* reduceval = (t->private_mats[i])->val + row_id * (mat->dim2);
-			#pragma omp simd
-			for(int y=0; y<r ; y++)
+			#pragma omp parallel for
+			for(int row_id = 0; row_id < mat->dim1 ; row_id++)
 			{
-				outval[y] += reduceval[y];
-				//printf("reducing %lf %lf in th %d\n", outval[y], reduceval[y],i);
-				//reduceval[y] = 0;
+				TYPE* outval = mat->val + row_id * (mat->dim2);
+				TYPE* reduceval = (t->private_mats[i])->val + row_id * (mat->dim2);
+				#pragma omp simd
+				for(int y=0; y<r ; y++)
+				{
+					outval[y] += reduceval[y];
+					//printf("reducing %lf %lf in th %d\n", outval[y], reduceval[y],i);
+					//reduceval[y] = 0;
+				}
+			}
+		}
+		else
+		{
+			for(int row_id = 0; row_id < mat->dim1 ; row_id++)
+			{
+				TYPE* outval = mat->val + row_id * (mat->dim2);
+				TYPE* reduceval = (t->private_mats[i])->val + row_id * (mat->dim2);
+				#pragma omp simd
+				for(int y=0; y<r ; y++)
+				{
+					outval[y] += reduceval[y];
+					//printf("reducing %lf %lf in th %d\n", outval[y], reduceval[y],i);
+					//reduceval[y] = 0;
+				}
 			}
 		}
 	}
