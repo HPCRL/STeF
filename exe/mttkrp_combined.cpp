a1b531240bd4b29834167f01fb60a0b0ff8f6205
Sureyya Emre Kurt <semrekurt@gmail.com>
2021-09-25 20:10:47 -0600

    LIKWID updates and mttkrp_combined_4 added

--- exe/mttkrp_combined.cpp
+++ exe/mttkrp_combined.cpp
@@ -61,184 +61,200 @@ int main(int argc, char** argv)
 	num_th = omp_get_max_threads();
 	#endif
 
-	if (nmode > 3)
-		return 0;
-
-	for(int mode = 0 ; mode<nmode ; mode++)
-	{
-		const bool intv = false;
-		clock_t cstart, cend;
-		auto start = std::chrono::high_resolution_clock::now();
-		cstart = clock();
-        if(mode > 0 && ((t->fiber_count[mode] / t->mlen[mode] < num_th * ATOMIC_THRESH) || (t->mlen[mode] * r * num_th >= PRIVATIZED_THRESH) ))	
-		{
-			if (mode == 0)
-				mttkrp_combined_3<0,false,false>(t,r,mats,profile);
-			else if (mode == 1)
-				mttkrp_combined_3<1,false,false>(t,r,mats,profile);
-			else if (mode == 2)
-				mttkrp_combined_3<2,false,false>(t,r,mats,profile);
-		}
-		else
-		{
-			if (mode == 0)
-				mttkrp_combined_3<0,false,true>(t,r,mats,profile);
-			else if (mode == 1)
-				mttkrp_combined_3<1,false,true>(t,r,mats,profile);
-			else if (mode == 2)
-				mttkrp_combined_3<2,false,true>(t,r,mats,profile);
-		}	
-		cend = clock();
-		//printf("here\n");
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> diff = end-start;
-		total += diff.count();
-		printf("Combined not saved time for mode %d %lf \n",t->modeid[mode],diff.count());
-		double cdiff = ((double) (cend - cstart)) / CLOCKS_PER_SEC;
-		printf("Clock time for mode %d is %lf \n",t->modeid[mode],cdiff);
-		if(debug)
-		{
-			auto start2 = std::chrono::high_resolution_clock::now();
-			mttkrp_test(dt,mode,r,mats);
-			auto end2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> diff = end2-start2;
-			//total += diff.count();
-			printf("COO sequential time for mode %d %lf \n",t->modeid[mode],diff.count());
-		}
-
-		for(i=0 ; i<nmode ; i++)
-		{
-			
-			if(VERBOSE  == VERBOSE_DEBUG)
-			{
-				print_matrix(*mats[i]);
-				
-			}
-			//random_matrix(*mats[i],i+1);
-			//set_matrix(*mats[i],1);
-		}
-		random_matrix(*mats[mode],mode);
-
-		
-	}
-	printf("Total Intermediate %s template MTTKRP time %lf\n",("not saved"),total);
-	total = 0;
-	for(int mode = 0 ; mode<nmode ; mode++)
-	{
-		const bool intv = true;
-		clock_t cstart, cend;
-		auto start = std::chrono::high_resolution_clock::now();
-		cstart = clock();
-		if(mode > 0 && ((t->fiber_count[mode] / t->mlen[mode] < num_th * ATOMIC_THRESH) || (t->mlen[mode] * r * num_th >= PRIVATIZED_THRESH) ))	
-		{
-			if (mode == 0)
-				mttkrp_combined_3<0,true,false>(t,r,mats,profile);
-			else if (mode == 1)
-				mttkrp_combined_3<1,true,false>(t,r,mats,profile);
-			else if (mode == 2)
-				mttkrp_combined_3<2,true,false>(t,r,mats,profile);
-		}
-		else
-		{
-			if (mode == 0)
-				mttkrp_combined_3<0,true,true>(t,r,mats,profile);
-			else if (mode == 1)
-				mttkrp_combined_3<1,true,true>(t,r,mats,profile);
-			else if (mode == 2)
-				mttkrp_combined_3<2,true,true>(t,r,mats,profile);
-		}	
-		cend = clock();
-		//printf("here\n");
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::duration<double> diff = end-start;
-		total += diff.count();
-		printf("Combined saved time for mode %d %lf \n",t->modeid[mode],diff.count());
-		double cdiff = ((double) (cend - cstart)) / CLOCKS_PER_SEC;
-		printf("Clock time for mode %d is %lf \n",t->modeid[mode],cdiff);
-		if(debug)
-		{
-			auto start2 = std::chrono::high_resolution_clock::now();
-			mttkrp_test(dt,mode,r,mats);
-			auto end2 = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> diff = end2-start2;
-			//total += diff.count();
-			printf("COO sequential time for mode %d %lf \n",t->modeid[mode],diff.count());
-		}
-
-		for(i=0 ; i<nmode ; i++)
-		{
-			
-			if(VERBOSE  == VERBOSE_DEBUG)
-			{
-				print_matrix(*mats[i]);
-				
-			}
-			//random_matrix(*mats[i],i+1);
-			//set_matrix(*mats[i],1);
-		}
-		random_matrix(*mats[mode],mode);
-		
-	}
-	printf("Total Intermediate %s template MTTKRP time %lf\n",( "saved" ),total);
-
-	/*
-	memset(t->intval[1],0,t->fiber_count[1]*r*sizeof(TYPE));
-
-
-    for (int repeat = 0 ; repeat < 2 ; repeat ++)
-    {
-        bool intv = false;
-        if ( repeat)
-            intv = true;
-        total = 0;
-        for(int mode = 0 ; mode<nmode ; mode++)
-        {
-            clock_t cstart, cend;
-            auto start = std::chrono::high_resolution_clock::now();
-            cstart = clock();
-            if (mode == 0)
-                mttkrp_combined_3(t,r,mats,profile,0,intv);
-            else if (mode == 1)
-                mttkrp_combined_3(t,r,mats,profile,1,intv);
-            else if (mode == 2)
-                mttkrp_combined_3(t,r,mats,profile,2,intv);
-            cend = clock();
-            //printf("here\n");
-            auto end = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> diff = end-start;
-            total += diff.count();
-            printf("Intermediate save is %s, combined time for mode %d %lf \n",(intv ? "on" : "off"),t->modeid[mode],diff.count());
-            double cdiff = ((double) (cend - cstart)) / CLOCKS_PER_SEC;
-            printf("Clock time for mode %d is %lf \n",t->modeid[mode],cdiff);
-            if(debug)
-            {
-                //  for(i=0 ; i<nmode ; i++)  if (i != mode)               random_matrix(*mats[i],i);
-                auto start2 = std::chrono::high_resolution_clock::now();
-                mttkrp_test(dt,mode,r,mats);
-                auto end2 = std::chrono::high_resolution_clock::now();
-                std::chrono::duration<double> diff = end2-start2;
-                //total += diff.count();
-                printf("COO sequential time for mode %d %lf \n",t->modeid[mode],diff.count());
-            }
-            random_matrix(*mats[mode],mode);
-
-            for(i=0 ; i<nmode ; i++)
-            {
-                //random_matrix(*mats[i],i);
-                
-                if(VERBOSE  == VERBOSE_DEBUG)
-                {
-                    print_matriif (mode == 0)
-		    mttkrp_combined_3<0,false>(t,r,mats,profile);
-        else if (mode == 1)
-            mttkrp_combined_3<1,false>(t,r,mats,profile);
-        else if (mode == 2)
-            mttkrp_combined_3<2,false>(t,r,mats,profile);
-        printf("Total Intermediate %s MTTKRP time %lf\n",(intv ? "saved" : "not saved"),total);
-    }
-	*/
 	
+		
+	if (nmode == 3)
+	{
+		for(int mode = 0 ; mode<nmode ; mode++)
+		{
+			const bool intv = false;
+			clock_t cstart, cend;
+			auto start = std::chrono::high_resolution_clock::now();
+			cstart = clock();
+			if(mode > 0 && ((t->fiber_count[mode] / t->mlen[mode] < num_th * ATOMIC_THRESH) || (t->mlen[mode] * r * num_th >= PRIVATIZED_THRESH) ))	
+			{
+				if (mode == 0)
+					mttkrp_combined_3<0,false,false>(t,r,mats,profile);
+				else if (mode == 1)
+					mttkrp_combined_3<1,false,false>(t,r,mats,profile);
+				else if (mode == 2)
+					mttkrp_combined_3<2,false,false>(t,r,mats,profile);
+			}
+			else
+			{
+				if (mode == 0)
+					mttkrp_combined_3<0,false,true>(t,r,mats,profile);
+				else if (mode == 1)
+					mttkrp_combined_3<1,false,true>(t,r,mats,profile);
+				else if (mode == 2)
+					mttkrp_combined_3<2,false,true>(t,r,mats,profile);
+			}	
+			cend = clock();
+			//printf("here\n");
+			auto end = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> diff = end-start;
+			total += diff.count();
+			printf("Combined not saved time for mode %d %lf \n",t->modeid[mode],diff.count());
+			double cdiff = ((double) (cend - cstart)) / CLOCKS_PER_SEC;
+			printf("Clock time for mode %d is %lf \n",t->modeid[mode],cdiff);
+			if(debug)
+			{
+				auto start2 = std::chrono::high_resolution_clock::now();
+				mttkrp_test(dt,mode,r,mats);
+				auto end2 = std::chrono::high_resolution_clock::now();
+				std::chrono::duration<double> diff = end2-start2;
+				//total += diff.count();
+				printf("COO sequential time for mode %d %lf \n",t->modeid[mode],diff.count());
+			}
 
+			for(i=0 ; i<nmode ; i++)
+			{
+				
+				if(VERBOSE  == VERBOSE_DEBUG)
+				{
+					print_matrix(*mats[i]);
+					
+				}
+				//random_matrix(*mats[i],i+1);
+				//set_matrix(*mats[i],1);
+			}
+			random_matrix(*mats[mode],mode);
+
+			
+		}
+		printf("Total Intermediate %s template MTTKRP time %lf\n",("not saved"),total);
+		total = 0;
+		for(int mode = 0 ; mode<nmode ; mode++)
+		{
+			const bool intv = true;
+			clock_t cstart, cend;
+			auto start = std::chrono::high_resolution_clock::now();
+			cstart = clock();
+			if(mode > 0 && ((t->fiber_count[mode] / t->mlen[mode] < num_th * ATOMIC_THRESH) || (t->mlen[mode] * r * num_th >= PRIVATIZED_THRESH) ))	
+			{
+				if (mode == 0)
+					mttkrp_combined_3<0,true,false>(t,r,mats,profile);
+				else if (mode == 1)
+					mttkrp_combined_3<1,true,false>(t,r,mats,profile);
+				else if (mode == 2)
+					mttkrp_combined_3<2,true,false>(t,r,mats,profile);
+			}
+			else
+			{
+				if (mode == 0)
+					mttkrp_combined_3<0,true,true>(t,r,mats,profile);
+				else if (mode == 1)
+					mttkrp_combined_3<1,true,true>(t,r,mats,profile);
+				else if (mode == 2)
+					mttkrp_combined_3<2,true,true>(t,r,mats,profile);
+			}	
+			cend = clock();
+			//printf("here\n");
+			auto end = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> diff = end-start;
+			total += diff.count();
+			printf("Combined saved time for mode %d %lf \n",t->modeid[mode],diff.count());
+			double cdiff = ((double) (cend - cstart)) / CLOCKS_PER_SEC;
+			printf("Clock time for mode %d is %lf \n",t->modeid[mode],cdiff);
+			if(debug)
+			{
+				auto start2 = std::chrono::high_resolution_clock::now();
+				mttkrp_test(dt,mode,r,mats);
+				auto end2 = std::chrono::high_resolution_clock::now();
+				std::chrono::duration<double> diff = end2-start2;
+				//total += diff.count();
+				printf("COO sequential time for mode %d %lf \n",t->modeid[mode],diff.count());
+			}
+
+			for(i=0 ; i<nmode ; i++)
+			{
+				
+				if(VERBOSE  == VERBOSE_DEBUG)
+				{
+					print_matrix(*mats[i]);
+					
+				}
+				//random_matrix(*mats[i],i+1);
+				//set_matrix(*mats[i],1);
+			}
+			random_matrix(*mats[mode],mode);
+			
+		}
+		printf("Total Intermediate %s template MTTKRP time %lf\n",( "saved" ),total);
+	}
+	else if (4 == 4)
+	{
+		int num_cases = 1;
+		for(int i = 0 ; i< 4-2; i++)
+			num_cases *= 2;
+		total = 0;
+		for(int mode = 0 ; mode<nmode ; mode++)
+		{
+			const bool intv1 = true;
+			const bool intv2 = true;
+			const int mode_c = mode;
+
+			clock_t cstart, cend;
+			auto start = std::chrono::high_resolution_clock::now();
+			cstart = clock();
+			if(mode > 0 && ((t->fiber_count[mode] / t->mlen[mode] < num_th * ATOMIC_THRESH) || (t->mlen[mode] * r * num_th >= PRIVATIZED_THRESH) ))	
+			{	// Atomic Update
+				if (mode == 0)
+					mttkrp_combined_4<0,intv1,false,false>(t,r,mats,profile);
+				else if (mode == 1)
+					mttkrp_combined_4<1,intv1,false,false>(t,r,mats,profile);
+				else if (mode == 2)
+					mttkrp_combined_4<2,intv1,false,false>(t,r,mats,profile);
+				else if (mode == 3)
+					mttkrp_combined_4<3,intv1,false,false>(t,r,mats,profile);
+			}
+			else
+			{ 	// Privatization
+				if (mode == 0)
+					mttkrp_combined_4<0,intv1,false,true>(t,r,mats,profile);
+				else if (mode == 1)
+					mttkrp_combined_4<1,intv1,false,true>(t,r,mats,profile);
+				else if (mode == 2)
+					mttkrp_combined_4<2,intv1,false,true>(t,r,mats,profile);
+				else if (mode == 3)
+					mttkrp_combined_4<3,intv1,false,true>(t,r,mats,profile);
+			}	
+			cend = clock();
+			//printf("here\n");
+			auto end = std::chrono::high_resolution_clock::now();
+			std::chrono::duration<double> diff = end-start;
+			total += diff.count();
+			printf("Combined not saved time for mode %d %lf \n",t->modeid[mode],diff.count());
+			double cdiff = ((double) (cend - cstart)) / CLOCKS_PER_SEC;
+			printf("Clock time for mode %d is %lf \n",t->modeid[mode],cdiff);
+			if(debug)
+			{
+				auto start2 = std::chrono::high_resolution_clock::now();
+				mttkrp_test(dt,mode,r,mats);
+				auto end2 = std::chrono::high_resolution_clock::now();
+				std::chrono::duration<double> diff = end2-start2;
+				//total += diff.count();
+				printf("COO sequential time for mode %d %lf \n",t->modeid[mode],diff.count());
+			}
+
+			for(i=0 ; i<nmode ; i++)
+			{
+				
+				if(VERBOSE  == VERBOSE_DEBUG)
+				{
+					print_matrix(*mats[i]);
+					
+				}
+				//random_matrix(*mats[i],i+1);
+				//set_matrix(*mats[i],1);
+			}
+			random_matrix(*mats[mode],mode);
+
+			
+		}
+
+		printf("Total Intermediate %s template MTTKRP time %lf\n",("not saved"),total);
+	}
 	
 	
 
--- inc/mttkrp_combined.h
+++ inc/mttkrp_combined.h
@@ -204,7 +204,10 @@ int mttkrp_combined_3(csf* t, int r, matrix** mats, int profile )
 	{
 		reduce(t,r,mats[mode]);
 	}
-	LIKWID_MARKER_CLOSE;
+	if(profile == mode)
+	{
+		LIKWID_MARKER_CLOSE;
+	}
 	rem(partial_results_all);	
 	return 0;
 	
@@ -212,8 +215,270 @@ int mttkrp_combined_3(csf* t, int r, matrix** mats, int profile )
 
 int mttkrp_combined_3(csf* t, int r, matrix** mats, int profile , int mode, bool intv1 );
 
-template <int mode, bool intv1, bool intv2>
-int mttkrp_combined_4(csf* t, int r, matrix** mats, int profile );
+template <int mode, bool intv1, bool intv2, bool privatized>
+int mttkrp_combined_4(csf* t, int r, matrix** mats, int profile )
+{
+	int nmode = t->nmode;
+	int num_th = 1;
+	int partial_results_size = nmode*r+PAD; idx_t* thread_start = t->thread_start;
+	#ifdef OMP
+	num_th = omp_get_max_threads();
+	#endif
+	
+	if(VERBOSE >= VERBOSE_DEBUG) printf("num ths %d\n", num_th);
+	
+	TYPE* partial_results_all = (TYPE*) malloc(num_th*partial_results_size*sizeof(TYPE));
+//	TYPE* dot_partial_results_all = (TYPE*) malloc(num_th*partial_results_size*sizeof(TYPE));
+	
+	for(int i = 0 ; i < num_th*partial_results_size ; i++)
+	{
+		partial_results_all[i] = 0;
+	}
+		
+	
+	if (mode > 0 && privatized)
+	{
+		for ( int i=0; i<num_th; i++)
+		{
+			memset(t->private_mats[i]->val ,0,sizeof(TYPE)*mats[mode]->dim1 * mats[mode]->dim2);
+		}
+	}
+	
+	mutex_array* mutex = t->mutex;
+
+	memset(mats[mode]->val,0,sizeof(TYPE)*mats[mode]->dim1 * mats[mode]->dim2);
+	
+	if(profile == mode)
+	{
+		if(VERBOSE >= VERBOSE_DEBUG) printf("profiling mode %d  == %d\n",profile, t->modeid[profile] );
+		LIKWID_MARKER_INIT;
+	}
+	
+	#ifdef OMP
+	#pragma omp parallel
+	#endif
+	{
+		if (profile == mode)
+		{
+			LIKWID_MARKER_THREADINIT;	
+		}
+	}
+	
+	#ifdef OMP
+	#pragma omp parallel
+	#endif
+	{
+		if(profile == mode)
+		{
+			LIKWID_MARKER_START("Compute");
+		}
+	}
+	
+	#ifdef OMP
+	#pragma omp parallel 
+	#endif
+	{
+		int th = 0;
+		#ifdef OMP
+		th = omp_get_thread_num();
+		#endif
+		auto time_start = std::chrono::high_resolution_clock::now();
+		TYPE* partial_results = partial_results_all + th*partial_results_size;
+		// TYPE* dot_partial_results = dot_partial_results_all + th*partial_results_size;
+
+		TYPE* pr0 = partial_results;
+		TYPE* pr1 = partial_results + r;
+
+
+		for(idx_t i0 = thread_start[th] ; i0 < thread_start[th+1] ; i0++)
+		{
+			TYPE* mv0 = mats[0]->val + ((mats[0]) -> dim2) * t->ind[0][i0];
+			for(idx_t i1 = t->ptr[0][i0] ; i1< t->ptr[0][i0+1]; i1++)
+			{
+				TYPE* mv1 = mats[1]->val + ((mats[1]) -> dim2) * t->ind[1][i1];
+				if(intv1)
+					pr0 = t->intval[1] + i1*r ; // Set the location for intermediate value for T(i_1,i_2)
+				else
+					memset(pr0,0,sizeof(TYPE)*r); // Reset the previous values otherwise
+
+
+				if(mode >= 2)
+				{
+					for(int y=0 ; y<r ; y++)
+					{
+						pr0[y] += mv0[y] * mv1[y];	// KrP step			
+					}
+				}
+
+				if( mode == 0 || mode > 1 || !intv1 )
+				{					
+					for(idx_t i2 = t->ptr[1][i1] ; i2< t->ptr[1][i1+1]; i2++)
+					{	
+						TYPE* mv2 = mats[2]->val + ((mats[2]) -> dim2) * t->ind[2][i2];						
+						if(intv2)
+							pr1 = t->intval[2] + i2*r; // Set the location for intermediate value for T(i_1,i_2,i_3)
+						else
+							memset(pr1,0,sizeof(TYPE)*r); // Reset the previous values otherwise							
+
+						if(mode > 2)
+						{
+							for(int y=0 ; y<r ; y++)
+							{
+								pr1[y] += pr0[y] * mv2[y] ;	// KrP step			
+							}
+						}
+
+						if( mode == 0 || mode > 2 || !intv2 )
+						{					
+							for(idx_t i3 = t->ptr[2][i2] ; i3< t->ptr[2][i2+1]; i3++)
+							{	
+								TYPE* mv3 = mats[3]->val + ((mats[3] -> dim2) * t->ind[3][i3]);
+								TYPE tval = t->val[i2];													
+							
+								if(mode < 3)
+								{				
+									// Do dot product TTM for mode 3					
+									TYPE* matval = mv3;
+									#pragma omp simd
+									for(int y=0 ; y<r ; y++)
+									{
+										pr1[y] += tval * matval[y];	// dot TTM step			
+									}
+								}
+								else if (privatized)
+								{                            
+									TYPE* matval = t->private_mats[th]->val + ((mats[3]) -> dim2) * t->ind[3][i3];
+									#pragma omp simd
+									for(int y=0 ; y<r ; y++)
+									{
+										matval[y] += tval * pr1[y];	// saxpy step			
+									}
+								}
+								else
+								{
+									TYPE* matval = mv3;
+									idx_t row_id = t->ind[3][i3] ; 
+									#ifdef OMP
+									mutex_set_lock(mutex,row_id);
+									#endif									
+									#pragma omp simd
+									for(int y=0 ; y<r ; y++)
+									{
+										matval[y] += tval * pr1[y];	// saxpy step			
+									}
+									#ifdef OMP
+									mutex_unset_lock(mutex,row_id);
+									#endif
+								}
+							}
+						} // End of loop for traversing mode 3
+
+
+						if(mode < 2)
+						{	// Do dot product TTV for mode 2							
+							#pragma omp simd
+							for(int y=0 ; y<r ; y++)
+							{
+								pr0[y] += pr1[y] * mv2[y];	// dot TTV step			
+							}
+						}
+						else if(mode == 2)
+						{	// MTTKRP for mode 2					
+							if (privatized)
+							{
+								TYPE* matval = t->private_mats[th]->val + ((mats[2]) -> dim2) * t->ind[2][i2];
+								#pragma omp simd
+								for(int y=0 ; y<r ; y++)
+								{
+									matval[y] += pr0[y] * pr1[y];	// saxpy step			
+								}
+							}
+							else
+							{
+								idx_t row_id = t->ind[2][i2] ; 
+								TYPE* matval = mv2;
+								#ifdef OMP
+								mutex_set_lock(mutex,row_id);
+								#endif
+								#pragma omp simd
+								for(int y=0 ; y<r ; y++)
+								{
+									matval[y] += pr0[y] * pr1[y];	// saxpy step			
+								}
+								#ifdef OMP
+								mutex_unset_lock(mutex,row_id);
+								#endif
+							}								
+						}
+
+					}	
+				} // End of the loop for traversing mode 2	
+				
+				if(mode < 1)
+				{
+					//TYPE* matval = mv1;
+					#pragma omp simd
+					for(int y=0 ; y<r ; y++)
+					{
+						mv0[y] += pr0[y] * mv1[y]; // dot TTV to compute mode 0
+					}
+				}
+				else if (mode == 1)
+				{	// MTTKRP for mode 1
+					if(privatized)
+					{
+						TYPE* matval = t->private_mats[th]->val + ((mats[1]) -> dim2) * t->ind[1][i1];
+						#pragma omp simd
+						for(int y=0 ; y<r ; y++)
+						{
+							matval[y] += pr0[y] * mv0[y]; // saxpy
+						}
+					}
+					else
+					{
+						TYPE* matval = mv1;
+						idx_t row_id = t->ind[1][i1] ; 
+						#ifdef OMP
+						mutex_set_lock(mutex,row_id);
+						#endif
+						#pragma omp simd
+						for(int y=0 ; y<r ; y++)
+						{
+							matval[y] += pr0[y] * mv0[y]; // saxpy
+						}
+						#ifdef OMP
+						mutex_unset_lock(mutex,row_id);
+						#endif
+					}
+					
+				}
+			} // End of loop for traversing mode 1
+		} // End of loop for traversing mode 0
+		
+
+
+		auto time_end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> time_diff = time_end-time_start;
+		
+		
+		if(VERBOSE >= VERBOSE_DEBUG) printf("Hardwired time for mode %d thread %d %lf \n",t->modeid[mode],th,time_diff.count());		
+		if(profile == mode)
+		{
+			LIKWID_MARKER_STOP("Compute");
+		}
+	} // End of parallel region
+	
+	
+	if (mode > 0 && privatized)
+	{
+		reduce(t,r,mats[mode]);
+	}
+
+	LIKWID_MARKER_CLOSE;
+	rem(partial_results_all);	
+	return 0;
+	
+}
 
 template <int mode, bool intv1, bool intv2, bool intv3>
 int mttkrp_combined_5(csf* t, int r, matrix** mats, int profile );
